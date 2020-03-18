4f5a256b33528723a97474fa17ce805dcccd1637
Emma Price <emma.price@theappbusiness.com>
2020-03-18 16:30:33 +0000

    updated port

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-console.log('Client side JS loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
 const messageTwo = document.querySelector('#message-two')
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -14,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
-    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    fetch(`/weather?address=${location}`).then(response => {
       response.json().then(data => {
             if (data.error) {
                 messageOne.textContent = data.error
@@ -25,9 +21,3 @@ weatherForm.addEventListener('submit', (e) => {
       })
     })
 })
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
