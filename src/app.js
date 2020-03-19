ab01ad45224bd8d1f1fafc9b26340ef708977bc8
Emma Price <ekprice01@gmail.com>
2020-03-19 10:23:48 +0000

    updated ports

--- src/app.js
+++ src/app.js
@@ -96,6 +96,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Up and running on port 3000')
+app.listen(port, () => {
+    console.log(`Up and running on ${port}`)
 })
