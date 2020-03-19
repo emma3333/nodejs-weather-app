d13243854ec2d3322bb91f380cb445a6266bd0cc
Emma Price <ekprice01@gmail.com>
2020-03-19 15:14:35 +0000

    updated styling of weather app

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,33 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-one')
-const messageTwo = document.querySelector('#message-two')
+const locationText = document.querySelector('#location')
+const weatherSummary = document.querySelector('#summary')
+const tempHigh = document.querySelector('#temp-high')
+const tempLow = document.querySelector('#temp-low')
+const chanceOfRain = document.querySelector('#rain')
+const container = document.querySelector('.container')
 
+container.classList.remove('container')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
     const location = search.value
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+    locationText.textContent = 'Loading...'
+    weatherSummary.textContent = ''
+    tempHigh.textContent = ''
     
     fetch(`/weather?address=${location}`).then(response => {
       response.json().then(data => {
+          console.log(data)
             if (data.error) {
-                messageOne.textContent = data.error
+                locationText.textContent = data.error
             } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                container.classList.add('container')
+                locationText.textContent = data.location
+                weatherSummary.textContent = `${data.forecast.summary} It is currently ${data.forecast.temperature}°C out.`
+                chanceOfRain.text = `${data.forecast.chanceOfRain}`
+                tempHigh.textContent = `Temperature High: ${data.forecast.tempHigh}°C`
+                tempLow.textContent = `Temperature Low: ${data.forecast.tempLow}°C`
             }
       })
     })
